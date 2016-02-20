fb3a717517f66d15d27e9633352a9f1951e9e908
Ashley <Ashley@Ashleys-MacBook-Air.local>
2016-02-20 14:32:51 -0600

    routine push

--- template-parts/bootstrap-carousel.php
+++ template-parts/bootstrap-carousel.php
@@ -1,52 +1,39 @@
-<section class="col-xs-12 col-sm-5 col-md-7">
-    <h2>Mini Gallery</h2>
-	  <?php 
-		$images = get_field('gallery');
-		$divider = 8; // # of items/thumbnails to show before closing the element and opening another
-		
-		if( $images ): ?>
-		
-		  <div id="mini-carousel" class="carousel slide" data-ride="carousel">			  
-			<ul class="carousel-inner">
-				<li class="item active">
-				<?php 
-					$total = count( $images );
-					$counter = 0;
-					foreach( $images as $image ): 
-						$counter++; ?>
-					
-					<a href="<?php echo $image['sizes']['large']; ?>" class="fancybox img-<?php echo $counter; ?>" rel="mini">
-	                    <img class="img-responsive" src="<?php echo $image['sizes']['thumbnail']; ?>" alt="<?php echo $image['title']; ?>" />
-	                </a>
-					<?php $current_position = $images->$image + 1; // current_post starts at 0 
-						//if position is equal to the divider and not the last result close the currently open div and start another
-						if (/* $image < $image->$total && */ $counter % $divider == 0) : ?>
-							</li><li class="item">							
+<section>
+	<h2>Posts Carousel</h2>
+	<?php
+		$args = array(
+			'cat' 			=> '2',
+			'post_status' 	=> 'publish',
+			'orderby'		=> 'title',
+			'order'			=> 'ASC'
+		);
+		$query = new WP_Query( $args );
+		$divider = 3; // # of items/thumbnails to show before closing the element and opening another ?>
+
+	<?php if( $query->have_posts() ) : ?>
+		<ul id="mini-carousel">
+			<li class="item col-sm-4">
+			    <?php while( $query->have_posts() ) : $query->the_post(); ?>
+					<h3><?php the_title(); ?></h3>
+					<div class="text">
+						<?php echo wp_trim_words( get_the_excerpt(), 30 ); ?>
+						<a class="more" href="<?php the_permalink(); ?>">Read more</a>
+					</div>
+					<?php $current_position = $query->current_post + 1; // current_post starts at 0 ?>
+					<?php //if position is equal to the divider and not the last result close the currently open div and start another
+						if ( $current_position < $query->found_posts && $current_position % $divider == 0 ) : ?>
+							</li><li class="item col-sm-4">
 					<?php endif; ?>
-				<?php endforeach; ?>
-			  	</li>
-			</ul>
-			
-			<!-- Controls -->
-			<a class="left carousel-control" href="#mini-carousel" role="button" data-slide="prev">
-				<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-				<span class="sr-only">Previous</span>
-			</a>
-			<a class="right carousel-control" href="#mini-carousel" role="button" data-slide="next">
-				<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-				<span class="sr-only">Next</span>
-			</a>
-		  </div>
-		<?php endif; ?>
+			    <?php endwhile; ?>
+			</li><!-- end .item -->
+		</ul>
+	<?php endif; ?>
 </section>
 
 <script>
 jQuery(document).ready(function($) {
 	$('#mini-carousel').carousel({
 	  interval: 6000
-	});
-	$('.fancybox').fancybox({
-		padding: 0
 	});
 });
 </script>
